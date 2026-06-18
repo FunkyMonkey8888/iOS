8e2fc94a24b41f528b52715c6552e535e79f6094
FunkyMonkey8888 <varshithgoud.09@gmail.com>
2026-06-18 13:55:43 +0530

    Initial Commit

---
+++ WeatherNetwork/WeatherNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNetworkApp.swift
+//  WeatherNetwork
+//
+//  Created by Survisreevarshith.10852498 on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
